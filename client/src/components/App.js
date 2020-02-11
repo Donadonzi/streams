90950dad635b7f176bd7fa61f18cb7611b5260dd
Donadonzi <donadonzi@gmail.com>
2020-02-11 15:28:44 -0500

    react-router boilerplate

---
+++ client/src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+
+const App = () => {
+    return (
+        <div>
+            My streaming app
+        </div>
+    );
+};
+
+export default App;
