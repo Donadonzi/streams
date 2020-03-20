fb2cb59890d485e3653ff4a46bef9c4ffb3da00f
Donadonzi <donadonzi@gmail.com>
2020-03-19 20:48:40 -0400

    making the first redux form for StreamCreate

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,9 +1,10 @@
 import { combineReducers } from 'redux';
+import { reducer as formReducer } from 'redux-form'; // Changed the name in order to avoid confusion since we might have different reducers in the app
 
 import authReducer from './authReducer';
 
 
 export default combineReducers({
-    auth: authReducer
-
+    auth: authReducer,
+    form: formReducer
 });
