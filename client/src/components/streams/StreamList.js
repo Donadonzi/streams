54282f58c40e602da852bc9ae1053d724df86562
Donadonzi <donadonzi@gmail.com>
2020-03-21 20:46:00 -0400

    conditional rendering of button for signed-in user

--- client/src/components/streams/StreamList.js
+++ client/src/components/streams/StreamList.js
@@ -1,39 +1,71 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 import { fetchStreams } from '../../actions';
 
 class StreamList extends React.Component {
-    componentDidMount() {
-        this.props.fetchStreams();
-    }
+  componentDidMount() {
+    this.props.fetchStreams();
+  }
 
-    renderList() {
-        return this.props.streams.map( stream => {
-            return (
-                <div className="item" key={stream.id}>
-                    <i className="large middle aligned icon film" />
-                    <div className="content">
-                        {stream.title}
-                        <div className="description">{stream.description}</div>
-                    </div>    
-                </div>    
-            );
-        });
+  // This is a helper function to decide whether to show the Edit/Delete buttons or not
+  renderAdmin(stream) {
+    if (stream.userId === this.props.currentUserId && stream.userId != null) { // I added the && part myself
+      return (
+        <div className="right floated content">
+          <button className="ui button primary">Edit</button>
+          <button className="ui button negative">Delete</button>
+        </div>
+      );
     }
+  }
 
-    render() {
-        return (
-            <div>
-                <h2>Streams</h2>
-                <div className="ui celled list">{this.renderList()}</div>
-            </div>
-        );
+  renderList() {
+    return this.props.streams.map(stream => {
+      return (
+        <div className="item" key={stream.id}>
+          {this.renderAdmin(stream)}
+          <i className="large middle aligned icon film" />
+          <div className="content">
+            {stream.title}
+            <div className="description">{stream.description}</div>
+          </div>
+        </div>
+      );
+    });
+  }
+
+  //This is a helper method to decide wether to show the Create Stream button or not, if the user is not signed in masalan
+  renderCreate() {
+    if (this.props.isSignedIn) {
+      return (
+        <div style={{ textAlign: 'right'}}>
+          <Link to="/streams/new" className="ui button green">
+            Create a new stream
+          </Link>
+        </div>
+      );
     }
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>Streams</h2>
+        <div className="ui celled list">{this.renderList()}</div>
+        {this.renderCreate()}
+      </div>
+    );
+  }
 }
 
 const mapStateToProps = state => {
-    return { streams: Object.values(state.streams) };
+  return {
+    streams: Object.values(state.streams),
+    currentUserId: state.auth.userId,
+    isSignedIn: state.auth.isSignedIn
+  };
 };
 
 export default connect(mapStateToProps, { fetchStreams })(StreamList);
