c1f9c0695164b708e0ef5ced2d9d8214cdb94671
Donadonzi <donadonzi@gmail.com>
2020-03-22 16:47:58 -0400

    making EditStream component

--- client/src/components/streams/StreamEdit.js
+++ client/src/components/streams/StreamEdit.js
@@ -1,15 +1,31 @@
+import _ from 'lodash';
 import React from 'react';
 import { connect } from 'react-redux';
 
-import { fetchStream } from '../../actions';
+import { fetchStream, editStream } from '../../actions';
+import StreamForm from './StreamForm';
 
 class StreamEdit extends React.Component {
   componentDidMount() {
     this.props.fetchStream(this.props.match.params.id);
   }
+
+  onSubmit = (formValues) => {  
+    this.props.editStream(this.props.match.params.id, formValues);
+  }
+
   render() {
-    console.log(this.props);
-    return <div>StreamEdit</div>;
+    if (!this.props.stream) {
+      return <div>Loading...</div>
+    }
+    return (
+      <div>
+        <h3>Edit a Stream</h3>
+        {/* This way we will load the entire stream object in the ReduxForm. But we don't wanna do that al technicality and the fact that only the changed fields should be passed to the API, not IDs. So we extract the properties that we do want to be changed. */}
+        {/* <StreamForm onSubmit={this.onSubmit} initialValues={this.props.stream}/> */}
+        <StreamForm onSubmit={this.onSubmit} initialValues={_.pick(this.props.stream, 'title', 'description')}/>
+      </div>
+    );
   }
 }    
 
@@ -17,4 +33,4 @@ const mapStateToProps = (state, ownProps) => {
   return { stream: state.streams[ownProps.match.params.id] };
 };
 
-export default connect(mapStateToProps, { fetchStream })(StreamEdit);
+export default connect(mapStateToProps, { fetchStream, editStream })(StreamEdit);
